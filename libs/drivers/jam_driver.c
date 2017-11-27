65dec98086cacdcf923386b46bd7bbce70eb2011
Manasye Shousen Bukit <32813070+manasye@users.noreply.github.com>
2017-11-27 20:49:31 +0700

    added driver jam(gpp la walau ga dipake)

---
+++ libs/drivers/jam_driver.c
@@ -0,0 +1,29 @@
+/* File: Driver untuk ADT JAM */
+
+#include "../jam.h"
+#include <stdio.h>
+
+int main()
+{
+	JAM J1, J2;
+
+	printf("TESTING JAM\n\n");
+	printf("Jam Sekarang: ");
+	GetCurrentJAM(&J1);
+	TulisJAM(J1);
+	printf("\n");
+	printf("dalam detik: %ld\n", JAMToDetik(J1));
+	printf("\nInput J2: ");
+	BacaJAM(&J2);
+	printf("Jam 2: ");
+	TulisJAM(J2);
+	printf("\n");
+	printf("Durasi dari Jam sekarang ke Jam 2: %ld\n", Durasi(J1,J2));
+	printf("dalam Jam: ");
+	TulisJAM(DetikToJAM(Durasi(J1,J2)));
+	printf("\n");
+	printf("Jam Sekarang + Jam2: ");
+	TulisJAM(AddJAM(J1,J2));
+	printf("\n");
+   	return 0;
+}
