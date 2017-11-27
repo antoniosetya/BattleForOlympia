86626ad37882f5d4d0e1c131ad6a74f0a97653a4
Manasye Shousen Bukit <32813070+manasye@users.noreply.github.com>
2017-11-27 21:56:26 +0700

    add comment

--- libs/drivers/jam_driver.c
+++ libs/drivers/jam_driver.c
@@ -9,15 +9,20 @@ int main()
 
 	printf("TESTING JAM\n\n");
 	printf("Jam Sekarang: ");
+	// Mendapat jam sekarang
 	GetCurrentJAM(&J1);
+	// Menulis Jam sekarang
 	TulisJAM(J1);
 	printf("\n");
+	// Konversi ke dalam detik
 	printf("dalam detik: %ld\n", JAMToDetik(J1));
 	printf("\nInput J2: ");
+	// Membaca jam dari user
 	BacaJAM(&J2);
 	printf("Jam 2: ");
 	TulisJAM(J2);
 	printf("\n");
+	// Menghitung durasi jam 1 dari jam skrg
 	printf("Durasi dari Jam sekarang ke Jam 2: %ld\n", Durasi(J1,J2));
 	printf("dalam Jam: ");
 	TulisJAM(DetikToJAM(Durasi(J1,J2)));
